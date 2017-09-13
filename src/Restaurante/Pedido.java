303c0ddecfe634b69e825e5d50e2c145248eea69
KVNgamer <KVNgamer@gmail.com>
2017-09-13 00:35:13 -0300

    att10

--- src/Restaurante/Pedido.java
+++ src/Restaurante/Pedido.java
@@ -1,9 +1,11 @@
 package Restaurante;
 
 public class Pedido {
-	Restaurante rest1=new Restaurante();
-	double ped1=rest1.prato1.getPreco();
-	double ped2=rest1.prato2.getPreco();
-	double ped3=rest1.prato3.getPreco();
+	private Restaurante rest1=new Restaurante();
+	
+	private double ped1=rest1.prato1.getPreco();
+	private double ped2=rest1.prato2.getPreco();
+	private double ped3=rest1.prato3.getPreco();
+	
 	double somaPedido=ped1+ped2+ped3;
 }
