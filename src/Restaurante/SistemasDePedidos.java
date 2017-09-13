88534bff9f5d2a0699eba89614f7a3f039051bf6
KVNgamer <KVNgamer@gmail.com>
2017-09-13 00:33:55 -0300

    att8

--- src/Restaurante/SistemasDePedidos.java
+++ src/Restaurante/SistemasDePedidos.java
@@ -5,6 +5,8 @@ public class SistemasDePedidos {
 	public static void main(String[] args) {
 		Restaurante rest1=new Restaurante();
 		rest1.mostracardapio();
+		rest1.fazPedido();
+		rest1.troco();
 
 	}
 
