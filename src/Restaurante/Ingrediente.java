3bb3ed2f003656e3df5148218ebfdfbfca06048b
KVNgamer <KVNgamer@gmail.com>
2017-09-13 00:17:59 -0300

    att7

--- src/Restaurante/Ingrediente.java
+++ src/Restaurante/Ingrediente.java
@@ -1,15 +1,16 @@
 package Restaurante;
 
 public class Ingrediente {
-	private String[] ingrediente=new String[4];
-	public String[] getIngrediente() {
-		return ingrediente;
+	private String[] ingred=new String[4];
+	
+	public String getIngred(int x) {
+		return ingred[x];
 	}
 	{
-	ingrediente[0] =" + carne";
-	ingrediente[1] =" + legume";
-	ingrediente[2] =" + verdura";
-	ingrediente[3] =" + tempero";
-	ingrediente[4] =" + acompanhamento";
+	ingred[0] =" + carne";
+	ingred[1] =" + legume";
+	ingred[2] =" + verdura";
+	ingred[3] =" + tempero";
+	ingred[4] =" + acompanhamento";
 	}
 }
